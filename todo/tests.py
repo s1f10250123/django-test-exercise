9fb5a71344b81f50b5bdce468c542d565c19f86c
1F10250123 <s1f102501237@iniad.org>
2026-06-18 12:55:23 +0900

    ソート機能を追加

--- todo/tests.py
+++ todo/tests.py
@@ -62,9 +62,35 @@ class TodoViewTestCase(TestCase):
 
     def test_index_post(self):
         client = Client()
-        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
+        data = {'title': 'Test Task', 'due_at': '2026-06-24 23:59:59'}
         response = client.post('/', data)
     
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
+
+    def test_index_get_order_post(self):
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2026, 6, 25)))
+        task1.save()
+        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2026, 7, 25)))
+        task2.save()
+        client = Client()
+        response = client.get('/?order=post')
+    
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertEqual(response.context['tasks'][0], task2)
+        self.assertEqual(response.context['tasks'][1], task1)
+
+    def test_index_get_order_due(self):
+        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2026, 6, 25)))
+        task1.save()
+        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2026, 7, 25)))
+        task2.save()
+        client = Client()
+        response = client.get('/?order=due')
+    
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertEqual(response.context['tasks'][0], task1)
+        self.assertEqual(response.context['tasks'][1], task2)
